acab7822b932509106870e0b39864504865c57df
Cappuccinomild <rkdqudh1208@naver.com>
2018-09-08 22:29:19 +0900

    prac

---
+++ app/music_list.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+'''
+악보바다 악보존재여부 판단후 있을경우 1 없으면 0
+
+'''
+
+#악보바다 악보존재여부
+def check_score(lists):
+    #http://www.akbobada.com/searchAll.html?searchKeyword=노래제목&searchFlag=10&searchSecod=&artistOrderBy=1
+
+    lists = lists.split('\n')
+
+    for i in range(len(lists)):
+        lists[i] = lists[i].replace(' ', '+')
+        lists[i] = 'http://www.akbobada.com/searchAll.html?searchKeyword='\
+        + lists[i] + '&searchFlag=10&searchSecod=&artistOrderBy=1'
+
+    for url in lists:
+        search_score(url)
+
+
+    return 0
+
+def search_score(url):  #검색
+
+        html = get_html(url)
+
+        soup = BeautifulSoup(html, 'html.parser')
+            
+        #akbo2 > table > tbody > tr:nth-child(1) > td.music > a
+        music_area = soup.select( 'td.music > a' )
+        
+        print(music_area)
+
+
+def extract_data():#검색한 코드를 분석
+    return 0
+
+
+
+
+def get_html(url): # html 코드를 가져온다
+    resp = requests.get(url)
+    html = resp.text
+
+    return html
+
+
+songs = input()
+
+check_score(songs)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,59 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 def keyboard(request):
     return JsonResponse(
         {
             "type" : "buttons",
-            "buttons" : ["학식", "내일의 학식", "도서관"]
+            #노래추천 
+            "buttons" : ["노래 추천", "우노반 일정"]
         }
     )
 
+@csrf_exempt
+def answer(requset):
+
+    json_str = (requset.body).decode('utf-8')
+    received_json = json.loads(json_str)
+    content_name = received_json['content']
+
+    if content_name == '노래 추천':
+
+        return JsonResponse({
+            'message': {
+                'text' : '노래 추천\n양식은 노래제목 - 가수 로 해주세요.'
+            },
+
+            'keyboard':{
+                'type' : 'buttons',
+                'buttons' : ['']
+            }
+        })
+
+    elif content_name == '우노반 일정':
+
+        return JsonResponse({
+            'message': {
+                'text' : '노래 추천\n양식은 노래제목 - 가수 로 해주세요.'
+            },
+
+            'keyboard':{
+                'type' : 'buttons',
+                'buttons' : ['']
+            }
+        })
+
+    else:
+        return JsonResponse({
+            'message': {
+                'text' : '다시 입력해주세요.'
+            },
+
+            'keyboard':{
+                'type' : 'buttons',
+                'buttons' : ["노래 추천", "우노반 일정"]
+            }
+        })
